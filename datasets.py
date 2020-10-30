226c8d84ffb08bb5e9ec055c6a153e60cb9904f1
sushruthn96 <sushruthn96@gmail.com>
2020-10-30 01:01:08 -0700

    Minor changes in dataset

--- datasets.py
+++ datasets.py
@@ -6,11 +6,16 @@ from torch.utils.data import Dataset
 
 
 class bird_dataset(Dataset):
-    def __init__(self):
+    # You can read the train_list.txt and test_list.txt files here.
+    def __init__(self,root,file_path):
         pass
 
     def __len__(self):
         raise ("Not implemented")
 
+    # Reshape image to (224,224).
+    # Try normalizing with imagenet mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225] or
+    # any standard normalization
+    # You can other image transformation techniques too
     def __getitem__(self, item):
         raise ("Not implemented")
