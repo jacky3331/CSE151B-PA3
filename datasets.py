7e5ed30bb425889c24d74036c31fa8bca513f88f
Jacky Dam <44416424+jacky3331@users.noreply.github.com>
2021-01-03 12:47:24 -0800

    Add files via upload

--- datasets.py
+++ datasets.py
@@ -11,20 +11,9 @@ from skimage import io, transform
 
 
 # Dataset class to preprocess your data and labels
-# You can do all types of transformation on the images in this class
-
-
 class bird_dataset(Dataset):
     # You can read the train_list.txt and test_list.txt files here.
     def __init__(self,root,file_path):
-        # pass
-        # f = open(root + file_path)
-        # Lines = f.readlines()
-        # my_data = [line.strip("\n").split(" ") for line in Lines]
-        # my_data = [[my_data[index][0], int(my_data[index][1])] for index in range(len(my_data))]
-        
-        # self.data = [[torch.tensor(np.array(Image.open(root + '/images/' + my_data[index][0]))), \
-        #     int(my_data[index][1])] for index in range(len(my_data))]
         self.data = pd.read_csv(
             root + file_path, sep=" ", names=["name", "class"], header = None)
         self.root = root + '/images/'
@@ -34,44 +23,23 @@ class bird_dataset(Dataset):
         self.length = len(self.data)
         return self.length
 
-    # Reshape image to (224,224).
-    # Try normalizing with imagenet mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225] or
-    # any standard normalization
-    # You can other image transformation techniques too
     def __getitem__(self, item):
-        # return self.data[item]
-        # if torch.is_tensor(item):
-        #     item = item.tolist()
         if torch.is_tensor(item):
             item = item.tolist()
         
         img_name = os.path.join(self.root,
                                 self.data.iloc[item, 0])
         
-
-#         image = io.imread(img_name)
-#         image = Image.convert('RGB')
-
         image = Image.open(img_name)
         image = image.convert('RGB')
 
         classification = self.data.iloc[item, 1:].values[0]
-        # classification = np.array([classification])
-        #reshape rgb image to 1 dimensional?
-        # landmarks = landmarks.astype('float').reshape(-1, 2)
-        data_transform = transforms.Compose([transforms.RandomResizedCrop(224),  transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-        image = data_transform(image)
-
-        # sample = {'image': image, 'classification': classification}
+#         data_transform = transforms.Compose([transforms.RandomResizedCrop(224),  transforms.RandomHorizontalFlip(), transforms.ToTensor(),
+#                                              transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        
+#         image = data_transform(image)
 
         sample = (image, classification)
 
 
         return sample
-    
-    # def normalization(self):
-
-
-    # Testing if the code works
-    # def printstuff(self):
-    #     return self.data[0][0]
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import torchvision.models as models
 
 # Defining your CNN model
 # We have defined the baseline model
@@ -13,17 +13,17 @@ class baseline_Net(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.b2 = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
+            nn.Conv2d(64, 128, 3), 
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True)
         )
         self.b3 = nn.Sequential(
-            nn.Conv2d(128, 128, 3),
+            nn.Conv2d(128, 128, 3), 
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True)
         )
         self.b4 = nn.Sequential(
-            nn.MaxPool2d((3, 3)),
+            nn.MaxPool2d((3, 3)), 
             nn.Conv2d(128, 256, 3, stride=2),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True)
@@ -47,3 +47,103 @@ class baseline_Net(nn.Module):
         out4 = self.fc2(self.fc1(out_flat))
 
         return out4
+
+class custom_Net(nn.Module):
+
+    def __init__(self, classes):
+        super(custom_Net, self).__init__()
+        self.b1 = nn.Sequential(
+            nn.Conv2d(3, 128, 3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.b2 = nn.Sequential(
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(128, 128, 3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.b3 = nn.Sequential(
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(128, 256, 3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.b4 = nn.Sequential(
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(256, 256, 3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.b5 = nn.Sequential(
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(256, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.b6 = nn.Sequential(
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(512, 256, 3, stride=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(256, 1024),
+            nn.Dropout(p=0.50),
+            nn.ReLU(inplace=True)
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, classes),
+        )
+
+    def forward(self, x):
+        out1 = self.b2(self.b1(x))
+        out2 = self.b4(self.b3(out1))    
+        out3 = self.b6(self.b5(out2))
+        out_avg = self.avg_pool(out3)
+        out_flat = out_avg.view(-1, 256)
+        out4 = self.fc2(self.fc1(out_flat))
+
+        return out4
+    
+class vgg_16_Net(nn.Module):
+
+    def __init__(self, classes, fine_tuning=False):
+        super(vgg_16_Net, self).__init__()
+        vgg = models.vgg16_bn(pretrained=True)
+        
+        for param in vgg.parameters():
+            param.requires_grad = fine_tuning
+            
+        self.features = vgg.features
+        self.classifier = vgg.classifier
+        num_ftrs = vgg.classifier[6].in_features
+        self.classifier[6] = nn.Linear(num_ftrs, classes)
+
+    def forward(self, x):
+        f = self.features(x).view(x.shape[0], -1)
+        y = self.classifier(f)
+        return y
+
+class resnet_Net(nn.Module):
+
+    def __init__(self, classes, fine_tuning=False):
+        super(resnet_Net, self).__init__()
+        resnet = models.resnet18(pretrained=True)
+        
+        for param in resnet.parameters():
+            param.requires_grad = fine_tuning
+            
+        self.classifier = resnet
+        num_ftrs = resnet.fc.in_features
+        self.classifier.fc = nn.Linear(num_ftrs, classes)
+
+    def forward(self, x):
+        y = self.classifier(x)
+        return y
+
